b75174b87b991f97e11b754bc0fd664f33f015d2
praveen3073 <pop3073@gmail.com>
2020-09-24 13:56:48 +0530

    Added: UC1 Employee Attendance

--- empwageprob.java
+++ empwageprob.java
@@ -3,5 +3,15 @@ import java.util.*;
 public class empwageprob {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		empAttendance();
+	}
+	private static void empAttendance()
+	{
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random())%2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
