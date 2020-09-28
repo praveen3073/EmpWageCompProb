41c59b0d2a8e57d8a21e5e6bdeaf2f52b7761b06
praveen3073 <pop3073@gmail.com>
2020-09-28 12:13:25 +0530

    Added: UC8 Company Class

---
+++ Company.java
@@ -0,0 +1,7 @@
+
+public class Company {
+	public int EMP_RATE_PER_HOUR = 20;
+	public int TOTAL_MONTHLY_WORKING_DAYS = 20;
+	public int TOTAL_MONTHLY_WORKING_HOURS = 100;
+	public String companyName;
+}
